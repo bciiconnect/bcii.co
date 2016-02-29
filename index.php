2786705527b964ac9bc022ee92b1430057272883
Tommaso Armstrong <tomm@tomma.so>
2016-02-29 22:56:56 +1100

    WIPs

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
                         <div class="card-bg bg-blue" style="background-image: url('assets/ball.jpg')"></div>
                         <div class="details">
                             <h1>BCII Ball</h1>
-                            <h2>Photos from our BCII Ball</h2>
                         </div>
                     </div>
                 </a>
@@ -37,7 +36,6 @@
                         <div class="card-bg bg-dark-blue" style="background-image: url('assets/about.jpg')"></div>
                         <div class="details">
                             <h1>About Us</h1>
-                            <h2>All about the BCII Connect</h2>
                         </div>
                     </div>
                 </a>
@@ -48,7 +46,6 @@
                         <div class="card-bg bg-green" style="background-image: url('assets/events.jpg')"></div>
                         <div class="details">
                             <h1>Upcoming events</h1>
-                            <h2>See what's on</h2>
                         </div>
                     </a>
                 </div>
