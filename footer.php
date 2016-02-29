ee08d31b8115ee3180691fa447e31b555696ef27
Tommaso Armstrong <tomm@tomma.so>
2016-02-29 15:29:43 +1100

    Hacky php to test design in a more DRY way

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="col-1-3">
+        <h1>BCII Connect</h1>
+        <p>BCII Connect is an affiliated club of ActivateClubs</p>
+    </div>
+    <!--<div class="col-1-3 nav">-->
+    <!--<a href="">About</a>-->
+    <!--<a href="">Contact</a>-->
+    <!--<a href="">Events</a>-->
+    <!--<a href="">Team</a>-->
+    <!--<a href="">Sponsors</a>-->
+    <!--<a href="">Blog</a>-->
+    <!--</div>-->
+    <!--<div class="col-1-3">-->
+    <!--x-->
+    <!--</div>-->
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BCII Connect</title>
+
+    <link rel="stylesheet" href="default.css"/>
+</head>
+<body>
+<header class="main-header">
+    <h1>BCII Connect</h1>
+    <nav>
+        <li>about</li>
+        <li>team</li>
+        <li>blog</li>
+        <li>contact</li>
+    </nav>
+</header>
