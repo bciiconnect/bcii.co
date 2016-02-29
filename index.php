0ad9606d6c0b9d2290718a72080cb23963e4b61a
Tommaso Armstrong <tomm@tomma.so>
2016-02-29 22:50:56 +1100

    WIPs

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="default.css" />
+</head>
+<body>
+    <div class="sidebar">
+        <img src="logo.png" alt="BCII Connect Logo" />
+        
+        <nav>
+            <a href="about.php">about</a>
+            <a href="events.php">events</a>
+            <a href="team.php">team</a>
+            <a href="sponsors.php">sponsors</a>
+            <a href="contact.php">contact</a>
+        </nav>
+    </div>
+    <main>
+        <div class="header bg-orange">
+            <h1>ote bag swag twee kogi, next level tofu selvage beard truffaut yr franzen meggings stumptown bitters.</h1>
+        </div>
+        <div class="row">
+            <div class="col-1-3">
+                <a href="ball.php">
+                    <div class="card">
+                        <div class="card-bg bg-blue" style="background-image: url('assets/ball.jpg')"></div>
+                        <div class="details">
+                            <h1>BCII Ball</h1>
+                            <h2>Photos from our BCII Ball</h2>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-1-3">
+                <a href="about.php">
+                    <div class="card">
+                        <div class="card-bg bg-dark-blue" style="background-image: url('assets/about.jpg')"></div>
+                        <div class="details">
+                            <h1>About Us</h1>
+                            <h2>All about the BCII Connect</h2>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-1-3">
+                <div class="card">
+                    <a href="events.php">
+                        <div class="card-bg bg-green" style="background-image: url('assets/events.jpg')"></div>
+                        <div class="details">
+                            <h1>Upcoming events</h1>
+                            <h2>See what's on</h2>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+</body>
+</html>
